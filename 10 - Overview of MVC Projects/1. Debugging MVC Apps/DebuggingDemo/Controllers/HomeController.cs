f73616d0c3d217e9b7df9a09a44530f0eb498ba1
Adrian Istrate <adr_istrate@yahoo.com>
2011-10-17 19:09:08 +0200

    End of Chapter 10.

---
+++ 10 - Overview of MVC Projects/1. Debugging MVC Apps/DebuggingDemo/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace DebuggingDemo.Controllers {
+
+    public class HomeController : Controller {
+
+public ActionResult Index() {
+
+    int firstVal = 10;
+    int secondVal = 0;
+    int thirdVal = 2;
+    int result = firstVal / thirdVal;
+
+    ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+    return View(result);
+}
+
+        public ActionResult About() {
+            return View();
+        }
+    }
+}
